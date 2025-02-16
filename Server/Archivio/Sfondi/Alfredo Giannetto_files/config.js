cd0aa5104f65193ccf5db9354976e86ae9bb3fa5
alfredogiannetto <alfredogiannetto5@gmail.com>
2025-02-16 01:03:14 +0100

    Uptade

--- Server/Archivio/Sfondi/Alfredo Giannetto_files/config.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
-  window._POSTHOG_REMOTE_CONFIG['phc_7RR1Apj72CYHXqPVLXq17sRiDWwKLUDYvaBC6fbIq9y'] = {
-    config: {"token": "phc_7RR1Apj72CYHXqPVLXq17sRiDWwKLUDYvaBC6fbIq9y", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": true, "captureDeadClicks": false, "capturePerformance": false, "autocapture_opt_out": false, "autocaptureExceptions": false, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "sessionRecording": false, "heatmaps": false, "surveys": [], "defaultIdentifiedOnly": true},
-    siteApps: []
-  }
-})();
